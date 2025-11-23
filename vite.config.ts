696f8bbb863d0100a76b7d56eb647fd8572759fe
CodeCraftingDevelopment <christophe.pauliac@codecrafting.fr>
2025-11-23 20:40:04 +0100

    Problème CORS sur ouverture depuis build index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 import { readFileSync } from 'fs'
 
 // Lire la version depuis package.json
@@ -7,7 +8,11 @@ const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './', // Chemins relatifs pour ouvrir index.html directement sans serveur
+  plugins: [
+    react(),
+    viteSingleFile(), // Inline tout (JS+CSS) dans un seul HTML pour compatibilité file://
+  ],
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
   },
